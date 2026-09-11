2be3cebf22baa937b2bc89e67434994907e56780
Aniket-ISTG <aniketsen13012006@gmail.com>
2026-09-11 23:31:51 +0530

    minor bug related to simulator is resolved

--- frontend/components/quantum-lab.tsx
+++ frontend/components/quantum-lab.tsx
@@ -4,7 +4,6 @@ import Link from "next/link";
 import { useState } from "react";
 import { useRouter } from "next/navigation";
 import { supabase } from "@/lib/quantum/supabase";
-import { CircuitEditor } from "@/components/quantum/circuit/circuit-editor";
 import { StateVector } from "@/components/quantum/visualization/state-vector";
 import { ProbabilityDistribution } from "@/components/quantum/visualization/probability-distribution";
 import { getLesson } from "@/data/lessons";
