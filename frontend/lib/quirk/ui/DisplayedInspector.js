c8c4c4a579dd08a845230c2c732dcddf7f0d1eee
Aniket-ISTG <aniketsen13012006@gmail.com>
2026-09-26 01:51:20 +0530

    Remove red red lines stuff

--- frontend/lib/quirk/ui/DisplayedInspector.js
+++ frontend/lib/quirk/ui/DisplayedInspector.js
@@ -320,9 +320,9 @@ class DisplayedInspector {
     }
 
     _drawHint(painter) {
-        this._drawHint_dragGatesOntoCircuit(painter);
-        this._drawHint_watchOutputsChange(painter);
-        this._drawHint_useControls(painter);
+        //this._drawHint_dragGatesOntoCircuit(painter);
+        //this._drawHint_watchOutputsChange(painter);
+        //this._drawHint_useControls(painter);
     }
 
     /**
